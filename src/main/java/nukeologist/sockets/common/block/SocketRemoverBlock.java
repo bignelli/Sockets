7dcaa15862e58ddb38a7f03a9b475a74dc18e0f8
Nukeologist <guilhermebignelli@terra.com.br>
2020-03-30 19:16:20 -0300

    Fix Socket Remover not dropping items when broken.
    Signed-off-by: Nukeologist <guilhermebignelli@terra.com.br>

--- src/main/java/nukeologist/sockets/common/block/SocketRemoverBlock.java
+++ src/main/java/nukeologist/sockets/common/block/SocketRemoverBlock.java
@@ -21,7 +21,9 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
@@ -31,6 +33,7 @@ import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
+import net.minecraftforge.items.CapabilityItemHandler;
 import nukeologist.sockets.common.registry.SocketsTileEntities;
 import nukeologist.sockets.common.tileentity.SocketRemoverTileEntity;
 
@@ -63,6 +66,24 @@ public class SocketRemoverBlock extends Block {
             return ActionResultType.SUCCESS;
         }
         return ActionResultType.FAIL;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (newState.getBlock() != state.getBlock()) {
+            final TileEntity te = worldIn.getTileEntity(pos);
+            if (te instanceof SocketRemoverTileEntity) {
+                te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(h -> {
+                    final int slots = h.getSlots();
+                    for (int i = 0; i < slots; i++) {
+                        final ItemStack stack = h.getStackInSlot(i);
+                        if (!stack.isEmpty()) InventoryHelper.spawnItemStack(worldIn, pos.getX(), pos.getY(), pos.getZ(), stack);
+                    }
+                });
+            }
+        }
+        super.onReplaced(state, worldIn, pos, newState, isMoving);
     }
 
     @Override
